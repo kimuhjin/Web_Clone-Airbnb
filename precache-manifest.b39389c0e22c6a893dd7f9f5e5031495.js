540a90bf6545b8fadefb04c997542601f2c082a9
Kimuhjin <ajin950714@naver.com>
2020-06-12 17:26:04 +0900

    Updates

---
+++ precache-manifest.b39389c0e22c6a893dd7f9f5e5031495.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fa9a698d1b13e66f26c0c81055ab3bc",
+    "url": "/Web_Clone-Airbnb/index.html"
+  },
+  {
+    "revision": "2092841f28696333d2ab",
+    "url": "/Web_Clone-Airbnb/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "de2566f758b2c769bdb1",
+    "url": "/Web_Clone-Airbnb/static/js/2.5b29b8e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Web_Clone-Airbnb/static/js/2.5b29b8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2092841f28696333d2ab",
+    "url": "/Web_Clone-Airbnb/static/js/main.42c9a79e.chunk.js"
+  },
+  {
+    "revision": "0d80fe3810c5b86c5b7a",
+    "url": "/Web_Clone-Airbnb/static/js/runtime-main.374eff8e.js"
+  }
+]);
