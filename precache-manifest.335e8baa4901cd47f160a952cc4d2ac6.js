e3f9e4bf0d1d7f7ff44009facb9f3436d65ae4ae
Kimuhjin <ajin950714@naver.com>
2020-06-13 22:09:36 +0900

    Updates

--- precache-manifest.335e8baa4901cd47f160a952cc4d2ac6.js
+++ precache-manifest.335e8baa4901cd47f160a952cc4d2ac6.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fb952b0d7902ec5707619eba98211853",
+    "revision": "d5b6d2ff4f72e95af0f7b2295a276834",
     "url": "/Web_Clone-Airbnb/index.html"
   },
   {
-    "revision": "c657b1d663a3eb8c7e7f",
+    "revision": "fe8b094dc7758bbca5b7",
     "url": "/Web_Clone-Airbnb/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "f11075f86223d164e884",
-    "url": "/Web_Clone-Airbnb/static/js/2.8e5b4e98.chunk.js"
+    "revision": "9621072c5f5bce90a9b3",
+    "url": "/Web_Clone-Airbnb/static/js/2.a1260383.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/Web_Clone-Airbnb/static/js/2.8e5b4e98.chunk.js.LICENSE.txt"
+    "url": "/Web_Clone-Airbnb/static/js/2.a1260383.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c657b1d663a3eb8c7e7f",
-    "url": "/Web_Clone-Airbnb/static/js/main.15047d97.chunk.js"
+    "revision": "fe8b094dc7758bbca5b7",
+    "url": "/Web_Clone-Airbnb/static/js/main.84fef9a8.chunk.js"
   },
   {
     "revision": "0d80fe3810c5b86c5b7a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Web_Clone-Airbnb/precache-manifest.7d10b2cf0da61587bf962dd4b78be636.js"
+  "/Web_Clone-Airbnb/precache-manifest.335e8baa4901cd47f160a952cc4d2ac6.js"
 );
 
 self.addEventListener('message', (event) => {
